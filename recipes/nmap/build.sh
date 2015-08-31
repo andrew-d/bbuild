18beb81dc4eac484ef61f5e9d668943c9c6008d2
Andrew Dunham <andrew@du.nham.ca>
2015-08-31 15:16:08 -0700

    Get (parts of) nmap building for Windows

--- recipes/nmap/build.sh
+++ recipes/nmap/build.sh
@@ -12,10 +12,38 @@ sums=(
 library=false
 binary=true
 
-dependencies=("openssl" "liblua" "zlib" "readline")
+dependencies=("openssl" "liblua" "zlib")
+if [[ "$BBUILD_TARGET_PLATFORM" != "windows" ]]; then
+    dependencies=( "${dependencies[@]}" "readline" )
+fi
 
 # Common variables.
 _builddir="$BBUILD_SOURCE_DIR/$pkgname-$pkgver"
+_openssl_version="$(cat "$BBUILD_DEPCONF_DIR"/openssl/.version)"
+_openssl_dir="$(cat "$BBUILD_DEPCONF_DIR"/openssl/.source-dir)"
+_openssl_dir="$_openssl_dir"/"openssl-${_openssl_version}"
+
+
+function prepare() {
+    cd "$_builddir"
+
+    if [[ "$BBUILD_TARGET_PLATFORM" = "windows" ]]; then
+        # Need to append `-lgdi32` to all instances of OPENSSL_LIBS
+        declare -r configure_files=$(find . -name configure.ac)
+        for cf in $configure_files; do
+            cd "$(dirname "$cf")"
+
+            info2 "Patching configure.ac in: $(dirname "$cf")"
+            sed -i \
+                -e 's|OPENSSL_LIBS="-lssl -lcrypto"|OPENSSL_LIBS="-lssl -lcrypto -lgdi32"|g' \
+                configure.ac
+            autoreconf -i || true
+            cd "$_builddir"
+        done
+
+        # TODO: anything more?
+    fi
+}
 
 
 function build() {
@@ -24,10 +52,14 @@ function build() {
     local pcapty
     case "${BBUILD_TARGET_PLATFORM}" in
         linux|android)
-            pcapty=linux
+            pcapty="linux"
             ;;
         darwin)
-            pcapty=bpf
+            pcapty="bpf"
+            ;;
+        windows)
+            _build_windows
+            return $?
             ;;
         *)
             error "Don't know what pcap type to use for platform: ${BBUILD_TARGET_PLATFORM}"
@@ -35,12 +67,7 @@ function build() {
             ;;
     esac
 
-    local openssl_dir openssl_version
-    openssl_version="$(cat "$BBUILD_DEPCONF_DIR"/openssl/.version)"
-    openssl_dir="$(cat "$BBUILD_DEPCONF_DIR"/openssl/.source-dir)"
-    openssl_dir="$openssl_dir"/"openssl-${openssl_version}"
-
-    debug "openssl_dir = ${openssl_dir}"
+    debug "_openssl_dir = ${_openssl_dir}"
 
     info2 "Configuring nmap"
     AR="${AR}" \
@@ -57,9 +84,47 @@ function build() {
         --without-nmap-update \
         --disable-universal \
         --with-pcap="${pcapty}" \
-        --with-openssl="${openssl_dir}" \
+        --with-openssl="${_openssl_dir}" \
         || return 1
 
+    # Don't build the libpcap.so file
+    if [[ -e libpcap/Makefile ]]; then
+        sed -i -e 's/shared\: /shared\: #/' libpcap/Makefile || return 1
+    fi
+
+    _patch_makefile_vars || return 1
+    _fix_libpcre || return 1
+
+    # Do the real build.
+    info2 "Building for real"
+    make || return 1
+}
+
+
+function package() {
+    cd "$_builddir"
+
+    declare -a files=("ncat/ncat")
+    if [[ "${BBUILD_TARGET_PLATFORM}" != "windows" ]]; then
+        files=( "${files[@]}" "nmap" "nping/nping" )
+    fi
+
+    local f fname
+    for f in "${files[@]}"; do
+        fname=$(basename "$f")
+        strip_helper \
+            "${f}" \
+            "${BBUILD_OUT_DIR}/${fname}${BBUILD_BINARY_EXT}" \
+            || return 1
+    done
+}
+
+
+######################################################################
+## Helper Functions
+
+
+function _patch_makefile_vars() {
     # We need to manually patch the 'AR' and 'RANLIB' variables in all the
     # Makefiles, since they don't appear to be set properly by the configure
     # script.
@@ -73,11 +138,11 @@ function build() {
             -e "s|RANLIB\\s*=\\s*ranlib\$|RANLIB = ${RANLIB}|g" \
             "$mf" || return 1
     done
+}
 
-    # Don't build the libpcap.so file
-    if [[ -e libpcap/Makefile ]]; then
-        sed -i -e 's/shared\: /shared\: #/' libpcap/Makefile || return 1
-    fi
+
+function _fix_libpcre() {
+    cd "$_builddir"
 
     # 'Build' libpcre (which may fail, but will create the appropriate
     # Makefile).
@@ -89,19 +154,126 @@ function build() {
         -e "s|AR\\s*=\\s*ar\$|AR = ${AR}|g" \
         libpcre/Makefile || return 1
 
-    # Do the real build.
-    info2 "Building for real"
-    make || return 1
+    return 0
 }
 
 
-function package() {
-    cd "$_builddir"
+function _build_windows() {
+    warn "Building nmap on Windows is a giant pile of hacks!"
+    warn "You should be aware that this may stop working at any random point, or crash randomly, etc."
+    sleep 2
 
-    local f fname
-    for f in nmap ncat/ncat nping/nping; do
-        fname=$(basename "$f")
-        cp "${f}${BBUILD_BINARY_EXT}" "$BBUILD_OUT_DIR"/"${fname}${BBUILD_BINARY_EXT}"
-        ${STRIP} "$BBUILD_OUT_DIR"/"${fname}${BBUILD_BINARY_EXT}"
-    done
+    info2 "Building nbase"
+    cd "$_builddir/nbase"
+    AR="${AR}" \
+    CC="${CC} -DDISABLE_NSOCK_PCAP ${BBUILD_STATIC_FLAGS}" \
+    CXX="${CXX} ${BBUILD_STATIC_FLAGS}" \
+    CFLAGS="${BBUILD_STATIC_FLAGS} ${CFLAGS:-}" \
+    CXXFLAGS="${BBUILD_STATIC_FLAGS} ${CXXFLAGS:-} -static-libstdc++" \
+    RANLIB="${RANLIB}" \
+    ./configure \
+        --host=${BBUILD_CROSS_PREFIX} \
+        --build=i686 \
+        --disable-universal \
+        --disable-ipv6 \
+        --without-libpcap \
+        --with-openssl="${_openssl_dir}" \
+        || return 1
+
+    sed -i \
+        -e 's|#include <WINCRYPT.H>|/* #include <WINCRYPT.H> */|g' \
+        -e '/#include "nbase_winconfig.h"/a #include <errno.h>' \
+        nbase_winunix.h \
+        || return 1
+
+    sed -i \
+        -e 's|^typedef unsigned __int.*$|/* Typedef removed */|g' \
+        -e 's|^typedef signed __int.*$|/* Typedef removed */|g' \
+        nbase_winconfig.h \
+        || return 1
+
+    # Append the given string at the end of the line containing 'OBJS ='
+    sed -i \
+        -e '/^OBJS =/ s/$/ ${LIBOBJDIR}nbase_winunix.o/' \
+        Makefile \
+        || return 1
+
+    _patch_makefile_vars || return 1
+    make || return 1
+
+
+    info2 "Building nsock"
+    cd "$_builddir/nsock/src"
+    AR="${AR}" \
+    CC="${CC} -DDISABLE_NSOCK_PCAP ${BBUILD_STATIC_FLAGS}" \
+    CXX="${CXX} ${BBUILD_STATIC_FLAGS}" \
+    CFLAGS="${BBUILD_STATIC_FLAGS} ${CFLAGS:-}" \
+    CXXFLAGS="${BBUILD_STATIC_FLAGS} ${CXXFLAGS:-} -static-libstdc++" \
+    RANLIB="${RANLIB}" \
+    ./configure \
+        --host=${BBUILD_CROSS_PREFIX} \
+        --build=i686 \
+        --disable-universal \
+        --disable-ipv6 \
+        --without-libpcap \
+        --with-openssl="${_openssl_dir}" \
+        || return 1
+
+    sed -i \
+        -e 's|Winsock2.h|winsock2.h|g' \
+        engine_poll.c \
+        netutils.c \
+        nsock_internal.h \
+        || return 1
+
+    _patch_makefile_vars || return 1
+    make || return 1
+
+    info2 "Building ncat"
+    cd "$_builddir/ncat"
+    AR="${AR}" \
+    CC="${CC} -DDISABLE_NSOCK_PCAP ${BBUILD_STATIC_FLAGS}" \
+    CXX="${CXX} ${BBUILD_STATIC_FLAGS}" \
+    CFLAGS="${BBUILD_STATIC_FLAGS} ${CFLAGS:-}" \
+    CXXFLAGS="${BBUILD_STATIC_FLAGS} ${CXXFLAGS:-} -static-libstdc++" \
+    RANLIB="${RANLIB}" \
+    ./configure \
+        --host=${BBUILD_CROSS_PREFIX} \
+        --build=i686 \
+        --disable-universal \
+        --disable-ipv6 \
+        --without-libpcap \
+        --with-openssl="${_openssl_dir}" \
+        || return 1
+
+    sed -i \
+        -e 's|WinDef.h|windef.h|g' \
+        sys_wrap.h \
+        || return 1
+
+    sed -i \
+        -e 's|ncat_posix.c|ncat_win.c ncat_exec_win.c|g' \
+        -e 's|ncat_posix.o|ncat_win.o ncat_exec_win.o|g' \
+        -e 's|-lpcap||g' \
+        -e '/^OPENSSL_LIBS/ s/$/ -lws2_32/' \
+        Makefile \
+        || return 1
+
+    sed -i \
+        -e 's|#include <openssl/applink.c>|/* Removed include */|g' \
+        ncat_ssl.c \
+        || return 1
+
+    _patch_makefile_vars || return 1
+    make ncat || return 1
+    return 0
+
+    # Don't build or configure libpcre.
+
+    _patch_makefile_vars || return 1
+    _fix_libpcre || return 1
+
+    # Do the real build.
+    info2 "Building for real"
+    make || return 1
 }
